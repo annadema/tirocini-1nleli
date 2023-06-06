7c9c496c9d1f4c3a7d59dd2e3c428ea986ea9277
annadema <annamaria.demartino@gmail.com>
2023-06-06 18:11:42 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tirocini',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
